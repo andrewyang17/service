d59bae5eecca669fee321d4f4246e02091555a27
andrewyang17 <yangzhisiangg@gmail.com>
2021-12-26 22:55:40 +0800

    testing errors

--- app/services/sales-api/v1/testgrp/testgrp.go
+++ app/services/sales-api/v1/testgrp/testgrp.go
@@ -1,15 +1,30 @@
 package testgrp
 
 import (
-	"fmt"
-	"go.uber.org/zap"
+	"context"
+	"github.com/andrewyang17/service/foundation/web"
+	"math/rand"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 type Handlers struct {
 	Log *zap.SugaredLogger
 }
 
-func (h Handlers) Test(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello world")
+func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		//return errors.New("untrusted error")
+		//return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+		return web.NewShutdownError("shutting down")
+	}
+
+	data := struct{
+		Status string
+	}{
+		Status: "ok",
+	}
+
+	return web.Response(ctx, w, http.StatusOK, data)
 }
