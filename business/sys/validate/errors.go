382a5fd0784f5b469b6d25e7f74d3950ca1c24e0
andrewyang17 <yangzhisiangg@gmail.com>
2022-01-02 00:00:35 +0800

    latest

--- business/sys/validate/errors.go
+++ business/sys/validate/errors.go
@@ -6,8 +6,6 @@ import (
 	"errors"
 )
 
-var ErrInvalidID = errors.New("ID is not in its proper form")
-
 // ErrorResponse is the form used for API responses from failure in the API.
 type ErrorResponse struct {
 	Error  string `json:"error"`
@@ -19,15 +17,27 @@ type ErrorResponse struct {
 type RequestError struct {
 	Err    error
 	Status int
-	Fields error
 }
 
 func NewRequestError(err error, status int) error {
-	return &RequestError{err, status, nil}
+	return &RequestError{err, status}
 }
 
 func (err *RequestError) Error() string {
 	return err.Err.Error()
+}
+
+func IsRequestError(err error) bool {
+	var re *RequestError
+	return errors.As(err, &re)
+}
+
+func GetRequestError(err error) *RequestError {
+	var re *RequestError
+	if !errors.As(err, &re) {
+		return nil
+	}
+	return re
 }
 
 type FieldError struct {
@@ -43,6 +53,27 @@ func (fe FieldErrors) Error() string {
 		return err.Error()
 	}
 	return string(d)
+}
+
+func (fe FieldErrors) Fields() map[string]string {
+	m := make(map[string]string)
+	for _, fld := range fe {
+		m[fld.Field] = fld.Error
+	}
+	return m
+}
+
+func IsFieldErrors(err error) bool {
+	var fe FieldErrors
+	return errors.As(err, &fe)
+}
+
+func GetFieldErrors(err error) FieldErrors {
+	var fe FieldErrors
+	if !errors.As(err, &fe) {
+		return nil
+	}
+	return fe
 }
 
 // Cause iterates through all the wrapped errors until the root
